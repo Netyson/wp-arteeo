8789958fe524fd7131b4e8f2dbeb4e66982368d6
Netyson <gabriel.figueiro@netyson.com>
2020-02-02 14:46:38 +0100

    add margin letft to content if aside nav is expandet

--- assets/js/src/aside.js
+++ assets/js/src/aside.js
@@ -1,9 +1,17 @@
 function togglediv( id ) {
-	const div = document.getElementById( id );
-
+    const div = document.getElementById( id ); // aside - nav
+    const entryContent = document.getElementsByClassName("entry-content"); // change margin of entry-content
+	 
 	if ( div.className === 'aside-navigation' ) {
-		div.className = 'aside-navigation--expanded';
+        div.className = 'aside-navigation--expanded';
+		for (var i = 0; i < entryContent.length; i++) {
+			entryContent[i].style.marginLeft = 10 + "%";
+		}
+       
 	} else if ( div.className === 'aside-navigation--expanded' ) {
 		div.className = 'aside-navigation';
+			for (var i = 0; i < entryContent.length; i++) {
+			entryContent[i].style.marginLeft = "auto";
+		}
 	}
 }
